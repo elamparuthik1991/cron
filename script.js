e3e01322031b08acd3e372e0baa68b541f3d188a
Elamparuthi Karunakaran <elamparuthik1991@gmail.com>
2021-03-18 02:00:15 +0530

    id correction

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ $(document).ready(function () {
     // ADD EMPLOYEE
     $("#add-employee-form").submit(function (event) {
         event.preventDefault();
-        db.collection('Cron').document($('#cron-mode').val()).add({
+        db.collection('Cron').doc($('#cron-mode').val()).set({
                 id: $('#cron-id').val(),
 				mode: $('#cron-mode').val(),
 				instrumentType: $('#cron-instrumentType').val(),
